3414f5c1ab45edb17cad6daadfe3eccde6041007
GloriaArackel <163562151+GloriaArackel@users.noreply.github.com>
2024-04-26 09:42:27 -0700

    Add files via upload

---
+++ Words corresponding digits.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int day;
+cou<<"Enter the day number";
+cin>>day;
+switch(day)
+{
+case1:cout<<"ONE";
+      break;
+case2:cout<<"TWO";
+      break;
+case3:cout<<"THREE";
+      break;
+case4:cout<<"FOUR";
+      break;
+case5:cout<<"FIVE";
+      break;
+case6:cout<<"SIX";
+      break;
+case7:cout<<"SEVEN";
+      break;
+case8:cout<<"EIGHT";
+      break;
+case9:cout<<"NINE";
+      break;
+default:cout<<"Invalid input";
+}
+return0;
+}
